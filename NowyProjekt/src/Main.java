a1aad2705a283870b0caa69e1883b8962b133247
MrArond <Jakubr14@gmail.com>
2023-10-05 09:51:34 +0200

    Kalkulator

--- NowyProjekt/src/Main.java
+++ NowyProjekt/src/Main.java
@@ -1,65 +1,108 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JakubRybczyński");
-        
 
-        int i = 2;
-        System.out.println(i);
-        double f = 2.0;
-        long l = 2L;
+        System.out.println("Kalkulator");
 
-        Integer i2 = null;
-        System.out.println(i2);
-        Double f2 = 2.0;
-        Long l2 = 2L;
+        System.out.println("Podaj pierwszą liczbe");
+        double a = pobieranie();
 
-        char w = 'X';
-        String name = "Jakub";
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
+        System.out.println("Podaj drugą liczbe");
+        double b = pobieranie();
 
-        int licznik = 0;
-        while (licznik <= 10) {
-            System.out.print(licznik);
-            if (licznik < 10) {
-                System.out.print(", ");
-            }
-            licznik++;
+        System.out.println("Podaj typ działania");
+        System.out.println();
+        System.out.println("1. Dodawanie");
+        System.out.println("2. Odejmowanie");
+        System.out.println("3. Mnożenie");
+        System.out.println("4. Dzielenie");
+        System.out.println("5. Potęga");;
+        int choice = pobieranieInt();
+
+        switch (choice) {
+            case 1:
+                dodawanie(a, b);
+                break;
+            case 2:
+                odejmowanie(a, b);
+                break;
+            case 3:
+                mnozenie(a, b);
+                break;
+            case 4:
+                dzielenie(a, b);
+                break;
+            case 5:
+                potega(a, b);
+                break;
         }
-        System.out.println();
+    }
 
-        int zmienna;
-        for (zmienna = 0; zmienna <= 20; zmienna++) {
-            System.out.print(zmienna);
-            if (zmienna < 20) {
-                System.out.print(", ");
+
+    public static double pobieranie() {
+        Scanner myObj = new Scanner(System.in);
+        while (true) {
+            String input = myObj.nextLine();
+            Pattern pattern = Pattern.compile("^[0-9]*$");
+            if (pattern.matcher(input).matches()) {
+                return Double.parseDouble(input);
+            } else {
+                System.out.println("Podaj liczbe");
             }
         }
-        System.out.println();
+    }
 
-        int druga = 0;
-        do {
-            System.out.print(druga);
-            if (druga < 10) {
-                System.out.print(", ");
+    public static int pobieranieInt() {
+        Scanner myObj = new Scanner(System.in);
+        while (true) {
+            String input = myObj.nextLine();
+            Pattern pattern = Pattern.compile("^[0-9]*$");
+            if (pattern.matcher(input).matches()) {
+                return Integer.parseInt(input);
+            } else {
+                System.out.println("Podaj liczbe");
             }
-            druga++;
-        } while (druga <= 10);
-        System.out.println();
-        a();
-        b();
-        c();
+        }
     }
 
+    public static double dodawanie(double liczba1, double liczba2) {
+        double w = liczba1 + liczba2;
+        System.out.println("Wynik wynosi: " + w);
+        return w;
+    }
 
-    public static string a(){
-        System.out.println("Func A");
+    public static double odejmowanie(double liczba1, double liczba2) {
+        double e = liczba1 - liczba2;
+        System.out.println("Wynik wynosi: " + e);
+        return e;
     }
-    protected static void b(){
-        System.out.println("Func B");
+
+    public static double mnozenie(double liczba1, double liczba2) {
+        double r = liczba1 * liczba2;
+        System.out.println("Wynik wynosi: " + r);
+        return r;
     }
-    private static void c(){
-        System.out.println("Func C");
+
+    public static double dzielenie(double liczba1, double liczba2) {
+        if (liczba1 == 0 || liczba2 == 0) {
+            System.out.println("Jedna z liczb jest równa 0. Nie można dzielić przez 0.");
+            return Double.NaN;
+        }
+        double t = liczba1 / liczba2;
+        System.out.println("Wynik wynosi: " + t);
+        return t;
     }
+
+    public static double potega(double liczba1, double liczba2) {
+        double y = Math.pow(liczba1, liczba2);
+        ;
+        System.out.println("Wynik wynosi: " + y);
+        double u = Math.sqrt(y);
+        System.out.println("Wynik wynosi: " + u);
+        return u;
+    }
+
 
 }
